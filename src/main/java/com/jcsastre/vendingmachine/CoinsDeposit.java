cf9c8c6c3fc0413280ac11ccd966502aaffd4b51
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2017-08-10 20:14:54 +0200

    CoinsDepositImplTests test failing.

--- src/main/java/com/jcsastre/vendingmachine/CoinsDeposit.java
+++ src/main/java/com/jcsastre/vendingmachine/CoinsDeposit.java
@@ -3,9 +3,32 @@ package com.jcsastre.vendingmachine;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * <p>Interface representing a deposit of coins.</p>
+ *
+ * <p>That deposit has no limit capacity.</p>
+ */
 public interface CoinsDeposit {
 
+    /**
+     *  <p>Accepts a coin.</p>
+     *
+     * @param coin Coin to insert.
+     */
     void insertCoin(Coin coin);
 
+    /**
+     * <p>Tries to release the specified amount based on deposited coins.</p>
+     *
+     * @param amountToProvideInCents Amount to provide as coins.
+     * @return The list of coins for the amount, or Optional.empty()
+     */
     Optional<List<Coin>> tryToReleaseAmount(Integer amountToProvideInCents);
+
+    /**
+     * <p>Sets the deposited coins replacing the previous ones.</p>
+     *
+     * @param coins The list of coins being the new current state of deposited coins.
+     */
+    void setCoins(List<Coin> coins);
 }
---
+++ src/main/java/com/jcsastre/vendingmachine/CoinsDepositImpl.java
@@ -0,0 +1,22 @@
+package com.jcsastre.vendingmachine;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CoinsDepositImpl implements CoinsDeposit {
+    @Override
+    public void insertCoin(Coin coin) {
+        //TODO
+    }
+
+    @Override
+    public Optional<List<Coin>> tryToReleaseAmount(Integer amountToProvideInCents) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void setCoins(List<Coin> coins) {
+        //TODO
+    }
+}
