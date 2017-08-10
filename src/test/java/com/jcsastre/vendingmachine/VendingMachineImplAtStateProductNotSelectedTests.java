36e61ea400be1fc0c7e4f87ac05ef156cd0f82b0
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2017-08-10 16:05:05 +0200

    VendingMachineImpl tests at NoProductSelected state for selectProduct command failing.

--- src/test/java/com/jcsastre/vendingmachine/VendingMachineImplAtStateProductNotSelectedTests.java
+++ src/test/java/com/jcsastre/vendingmachine/VendingMachineImplAtStateProductNotSelectedTests.java
@@ -33,7 +33,7 @@ public class VendingMachineImplAtStateProductNotSelectedTests {
     }
 
     @Test
-    public void Given_ProductNotSelectedAndNoBalance_WhenInsertingCoin_Then_CorrectlyUpdateBalance()
+    public void Given_NoBalance_WhenInsertingCoin_Then_CorrectlyUpdateBalance()
         throws NoChangeException {
 
         // Given: Product Not Selected And NoBalance
@@ -50,7 +50,7 @@ public class VendingMachineImplAtStateProductNotSelectedTests {
     }
 
     @Test
-    public void Given_ProductNotSelectedAndBalance_WhenInsertingCoin_Then_CorrectlyUpdateBalance()
+    public void Given_Balance_WhenInsertingCoin_Then_CorrectlyUpdateBalance()
         throws NoChangeException {
 
         // Given: Product Not Selected And Balance
@@ -65,5 +65,102 @@ public class VendingMachineImplAtStateProductNotSelectedTests {
         assertThat(vendingMachineImpl.readSelectedProductIndicator(), is(Optional.empty()));
         assertThat(vendingMachineImpl.collectCoinsAtRepaymentPort(), is(Optional.empty()));
         assertThat(vendingMachineImpl.collectProductAtTakeoutPort(), is(Optional.empty()));
+    }
+
+    @Test // 1.2.1
+    public void Given_NoStockOfAProduct_When_SelectingThatProduct_Then_CorrectlyThrowNoStockException()
+        throws NoChangeException, NoStockException {
+
+        // Given: No Stock Of A Product
+        final VendingMachineImpl vendingMachineImpl = new VendingMachineImpl(coinsDeposit, productsDeposit);
+        when(productsDeposit.checkThereIsProductStock(Product.COKE)).thenReturn(false);
+
+        // When: Selecting That Product
+        com.googlecode.catchexception.apis.BDDCatchException.when(vendingMachineImpl).selectProduct(Product.COKE);
+
+        // Then: Correctly Throw NoStockException
+        then(caughtException()).isInstanceOf(NoStockException.class);
+        assertThat(vendingMachineImpl.readBalanceInCentsIndicator(), is(0));
+        assertThat(vendingMachineImpl.readSelectedProductIndicator(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.collectCoinsAtRepaymentPort(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.collectProductAtTakeoutPort(), is(Optional.empty()));
+    }
+
+    @Test // 1.2.2
+    public void Given_NoBalanceEnoughForAProduct_When_SelectingThatProduct_Then_CorrectlyUpdateSelectedProduct()
+        throws NoChangeException, NoStockException {
+
+        // Given: No Balance Enough For A Product
+        final VendingMachineImpl vendingMachineImpl = new VendingMachineImpl(coinsDeposit, productsDeposit);
+        when(productsDeposit.checkThereIsProductStock(Product.COKE)).thenReturn(true);
+
+        // When: Selecting That Product
+        vendingMachineImpl.selectProduct(Product.COKE);
+
+        // Then: Correctly Update Selected Product
+        assertThat(vendingMachineImpl.collectProductAtTakeoutPort(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.collectCoinsAtRepaymentPort(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.readBalanceInCentsIndicator(), is(0));
+        assertThat(vendingMachineImpl.readSelectedProductIndicator(), is(Optional.of(Product.COKE)));
+    }
+
+    @Test // 1.2.3
+    public void Given_ExactBalanceForAProduct_When_SelectingThatProduct_Then_CorrectlyReleaseThatProduct()
+        throws NoChangeException, NoStockException {
+
+        // Given: Exact Balance For A Product
+        final VendingMachineImpl vendingMachineImpl = new VendingMachineImpl(coinsDeposit, productsDeposit);
+        when(productsDeposit.checkThereIsProductStock(Product.COKE)).thenReturn(true);
+        Whitebox.setInternalState(vendingMachineImpl,"currentBalanceInCents",Product.COKE.getPriceInCents());
+
+        // When: Selecting That Product
+        vendingMachineImpl.selectProduct(Product.COKE);
+
+        // Then: Correctly Release That Product
+        assertThat(vendingMachineImpl.collectProductAtTakeoutPort(), is(Optional.of(Product.COKE)));
+        assertThat(vendingMachineImpl.collectCoinsAtRepaymentPort(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.readBalanceInCentsIndicator(), is(0));
+        assertThat(vendingMachineImpl.readSelectedProductIndicator(), is(Optional.empty()));
+    }
+
+    @Test // 1.2.4
+    public void Given_MoreThanRequiredBalanceForAProductButNoChangeAvailable_When_SelectingThatProduct_Then_CorrectlyThrowNoChangeExceptionReturnCoin()
+        throws NoChangeException, NoStockException {
+
+        // Given: More Than Required Balance For A Product But No ChangeAvailable
+        final VendingMachineImpl vendingMachineImpl = new VendingMachineImpl(coinsDeposit, productsDeposit);
+        when(productsDeposit.checkThereIsProductStock(Product.COKE)).thenReturn(true);
+        Whitebox.setInternalState(vendingMachineImpl,"currentBalanceInCents", 200);
+        when(coinsDeposit.tryToReleaseAmount(50)).thenReturn(Optional.empty());
+
+        // When: Selecting That Product
+        com.googlecode.catchexception.apis.BDDCatchException.when(vendingMachineImpl).selectProduct(Product.COKE);
+
+        // Then: Correctly Throw NoStockException
+        then(caughtException()).isInstanceOf(NoChangeException.class);
+        assertThat(vendingMachineImpl.readBalanceInCentsIndicator(), is(200));
+        assertThat(vendingMachineImpl.readSelectedProductIndicator(), is(Optional.of(Product.COKE)));
+        assertThat(vendingMachineImpl.collectCoinsAtRepaymentPort(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.collectProductAtTakeoutPort(), is(Optional.empty()));
+    }
+
+    @Test // 1.2.5
+    public void Given_MoreThanRequiredBalanceForAProduct_When_SelectingThatProduct_Then_CorrectlyReleaseTheProduct()
+        throws NoChangeException, NoStockException {
+
+        // Given: More Than Required Balance For A Product
+        final VendingMachineImpl vendingMachineImpl = new VendingMachineImpl(coinsDeposit, productsDeposit);
+        when(productsDeposit.checkThereIsProductStock(Product.COKE)).thenReturn(true);
+        Whitebox.setInternalState(vendingMachineImpl,"currentBalanceInCents", 200);
+        when(coinsDeposit.tryToReleaseAmount(50)).thenReturn(Optional.of(Collections.singletonList(Coin.FIFTY_CENTS)));
+
+        // When: Selecting That Product
+        vendingMachineImpl.selectProduct(Product.COKE);
+
+        // Then: Correctly Release The Product
+        assertThat(vendingMachineImpl.readBalanceInCentsIndicator(), is(0));
+        assertThat(vendingMachineImpl.readSelectedProductIndicator(), is(Optional.empty()));
+        assertThat(vendingMachineImpl.collectCoinsAtRepaymentPort(), is(Optional.of(Collections.singletonList(Coin.FIFTY_CENTS))));
+        assertThat(vendingMachineImpl.collectProductAtTakeoutPort(), is(Optional.of(Product.COKE)));
     }
 }
